e070575dcb293d1682534f2bb0ed365048510754
Himanshu Singh <himanshusingh2407@gmail.com>
2019-03-11 02:40:57 +0530

    Add build files

---
+++ precache-manifest.731c3b5ff5e28a46d0d0947190cea4c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a46ebb7b4428e6cf945c",
+    "url": "/hacker-news/static/css/main.74cf4e3c.chunk.css"
+  },
+  {
+    "revision": "a46ebb7b4428e6cf945c",
+    "url": "/hacker-news/static/js/main.0919d4fb.chunk.js"
+  },
+  {
+    "revision": "388ec0cd24e6422163cb",
+    "url": "/hacker-news/static/js/runtime~main.d8b6cfce.js"
+  },
+  {
+    "revision": "8529b7abd7730255fd8c",
+    "url": "/hacker-news/static/js/2.39393e23.chunk.js"
+  },
+  {
+    "revision": "b720f72c5f18326244eb488521a48326",
+    "url": "/hacker-news/index.html"
+  }
+];
