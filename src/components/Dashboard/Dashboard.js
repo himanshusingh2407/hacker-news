6e4b765792739c72914bf55dd41df08e3c81ee8a
Himanshu Singh <himanshusingh2407@gmail.com>
2019-03-10 17:52:43 +0530

    Add axios

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NewsList from '../NewsList/NewsList';
 import Pagination from '../Pagination/Pagination';
+import Axios from 'axios';
 import './Dashboard.css';
 
 class Dashboard extends Component {
@@ -12,6 +13,12 @@ class Dashboard extends Component {
         <Pagination />
       </div>
     );
+  }
+
+  componentDidMount() {
+    Axios.get('https://hacker-news.firebaseio.com/v0/topstories.json').then((response) => {
+      console.log(response.data);
+    });
   }
 }
 
